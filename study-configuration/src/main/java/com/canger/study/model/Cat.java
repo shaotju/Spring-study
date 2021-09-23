00f003992a0fb65ecfa04092aa4c5257d0edfc68
shaoxm <xinming.shao@its.cn>
2021-09-23 16:33:42 +0800

    test git master

--- study-configuration/src/main/java/com/canger/study/model/Cat.java
+++ study-configuration/src/main/java/com/canger/study/model/Cat.java
@@ -5,6 +5,15 @@ package com.canger.study.model;
  */
 public class Cat {
     private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public String getName() {
         return name;
