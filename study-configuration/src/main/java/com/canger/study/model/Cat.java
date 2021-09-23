9e7799716003a501aef220a9fc63ad51661f4e23
shaoxm <xinming.shao@its.cn>
2021-09-23 16:33:18 +0800

    test git master

--- study-configuration/src/main/java/com/canger/study/model/Cat.java
+++ study-configuration/src/main/java/com/canger/study/model/Cat.java
@@ -4,4 +4,13 @@ package com.canger.study.model;
  * testttttt git
  */
 public class Cat {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
