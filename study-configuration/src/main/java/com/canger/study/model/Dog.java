fb1e60ba0965df224709a08182e89ca166ee0a1d
shaoxm <xinming.shao@its.cn>
2021-09-24 13:41:43 +0800

    test git test

--- study-configuration/src/main/java/com/canger/study/model/Dog.java
+++ study-configuration/src/main/java/com/canger/study/model/Dog.java
@@ -2,6 +2,15 @@ package com.canger.study.model;
 
 public class Dog {
     private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public String getName() {
         return name;
